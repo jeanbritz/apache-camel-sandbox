59daeb45298ad46eaceac97d33e08b7e1b315707
JeanBritz <britz.jean@gmail.com>
2017-09-22 22:44:04 +0200

    Init Spring Maven Project

---
+++ src/main/java/com/britz/apachecamelsandbox/ApacheCamelSandboxApplication.java
@@ -0,0 +1,12 @@
+package com.britz.apachecamelsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelSandboxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/britz/apachecamelsandbox/ApacheCamelSandboxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.britz.apachecamelsandbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApacheCamelSandboxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
